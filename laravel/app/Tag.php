2db2896bbdcff78af5b3f388692978a2da46775a
nozomi <kkk@docomo.ne.jp>
2021-03-04 12:35:12 +0900

    タグ一覧画面実装

--- laravel/app/Tag.php
+++ laravel/app/Tag.php
@@ -3,6 +3,8 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Tag extends Model
 {
@@ -12,5 +14,10 @@ class Tag extends Model
     public function getHashtagAttribute(): string
     {
         return "#" . $this->name;
+    }
+
+    public function articles(): BelongsToMany
+    {
+        return $this->belongsToMany("App\Article")->withTimestamps();
     }
 }
